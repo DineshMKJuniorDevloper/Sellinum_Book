26a86d09c1c01e1a4f19c34f09872fe62b4e4515
dkm <dkm@dkm-HP-Notebook>
2024-05-25 16:31:52 +0530

    intial commit

---
+++ Book_Download_List/src/test/java/Books/Book_Download_List/BooksList.java
@@ -0,0 +1,14 @@
+package Books.Book_Download_List;
+
+
+
+class BookList {
+
+	public static void main(String[] args) throws InterruptedException  {
+	//	BookListPage bookList = new BookListPage();	
+	//	bookList.BooksDownloadListByAuthor("விக்னேஷ் .m");
+		
+		BookListPage.BooksDownloadListByAuthor("கி . நடராஜன்");
+	}
+
+}
